d5325e3b8ae79c592b8cf064d1482e93a15d5a38
mz7701 <korean2071@naver.com>
2022-06-01 23:20:28 +0900

    ss

--- server/server.js
+++ server/server.js
@@ -8,11 +8,11 @@ const path = require("path");
 const app = express()
 
 app.use(myconn(mysql, {
-    host: 'localhost',
-    port: 3307,
-    user: 'user.javafx',
-    password: 'Jahr2001#1!10.',
-    database: 'prueba'
+    host: 'database-3.cnoxhuocv4d1.us-west-2.rds.amazonaws.com',
+    port: 3306,
+    user: "moroju109",
+    password: 'xkqtop12',
+    database: 'img'
 }))
 app.use(cors())
 app.use(express.static(path.join(__dirname,'dbimages')))
